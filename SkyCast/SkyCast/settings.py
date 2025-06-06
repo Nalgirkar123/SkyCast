1e7a7bc398168f828da75722be3858d39ccd5818
omkar-dev <omkarnalgirkar17@gmail.com>
2025-06-06 16:59:59 +0530

    Prepare for Render deployment

--- SkyCast/SkyCast/settings.py
+++ SkyCast/SkyCast/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-2o+i_$q5=jt463j6#_ok&r+p7#4nzbfq82$!tg(3ht(^mr*@sm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['SkyCast.onrender.com']
 
 
 # Application definition
@@ -121,6 +122,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
