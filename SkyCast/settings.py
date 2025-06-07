27cf85094e667800b4b52bf28686f19d56726289
omkar-dev <omkarnalgirkar17@gmail.com>
2025-06-07 19:11:39 +0530

    WIP: Save changes before rebase

--- SkyCast/settings.py
+++ SkyCast/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'SkyCast.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'SkyCast_app' / 'templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'SkyCast_app', 'templates')],
 
         'APP_DIRS': True,
         'OPTIONS': {
@@ -118,10 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'SkyCast_app', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
